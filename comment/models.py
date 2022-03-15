a3b6c9c1ced301897a242b9a4142ab3cf7c750ad
Yongsen Luo <2612758L@student.gla.ac.uk>
2022-03-15 13:07:40 +0000

    simply comment version 1

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from theme.models import Article
 
 # Create your models here.
+class Comment(models.Model):
+    article = models.ForeignKey(verbose_name="belong to which article", to=Article, on_delete=models.CASCADE)
+    comment_id = models.AutoField(verbose_name="comment id", primary_key=True)
+    comment_text = models.TextField()
+    comment_time = models.DateTimeField(auto_now_add=True)
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    count_all_liked = models.IntegerField(verbose_name="how many person liked", default=0)
+    count_all_collected = models.IntegerField(verbose_name="how many person collected", default=0)
+    pre_comment = models.ForeignKey('self',on_delete=models.DO_NOTHING,null=True,verbose_name='父评论id')  #父级评论，如果没有父级则为空NULL, "self"表示外键关联自己
--- comment/views.py
+++ comment/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from .models import Comment
 
 # Create your views here.
 
@@ -15,4 +16,25 @@ from theme.models import Article
 
 def article_detail(request, article_id):
     article = Article.objects.get(pk=article_id)
-    return render(request, "article/publish_ar_detail.html", {"article": article})
+    comment_list = Comment.objects.filter(article_id = article_id)
+    context = {
+        'article' : article,
+        'comment_list' : comment_list
+    }
+    return render(request, "article/publish_ar_detail.html", context=context)
+
+@login_required
+@csrf_exempt
+def comment_control(request):    #提交评论的处理函数
+    if request.user.username:
+        comment_user = request.user
+        comment_text = request.POST.get('comment_text','')
+        article_id = request.POST.get('article_id')
+        pid = request.POST.get('pid')
+        Comment.objects.create(comment_text=comment_text,pre_comment_id=pid,article_id=article_id,comment_user=comment_user)
+
+        article = list(Comment.objects.values('comment_id','comment_text','pre_comment_id','article_id','comment_user','comment_time'))  #以键值对的形式取出评论对象，并且转化为列表list类型
+
+        return JsonResponse(article,safe=False)   #JsonResponse返回JSON字符串，自动序列化，如果不是字典类型，则需要添加safe参数为False
+    #else:
+    #    return redirect('/user_login/')
