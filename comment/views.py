314282bff283d6b1473fb29a0aa2ae681ec6bf2a
Yongsen Luo <2612758L@student.gla.ac.uk>
2022-03-15 13:25:05 +0000

    version 2

--- comment/views.py
+++ comment/views.py
@@ -28,13 +28,12 @@ def article_detail(request, article_id):
 def comment_control(request):    #提交评论的处理函数
     if request.user.username:
         comment_user = request.user
-        comment_text = request.POST.get('comment_text','')
+        comment_text = request.POST.get('text','')
         article_id = request.POST.get('article_id')
         pid = request.POST.get('pid')
         Comment.objects.create(comment_text=comment_text,pre_comment_id=pid,article_id=article_id,comment_user=comment_user)
 
         article = list(Comment.objects.values('comment_id','comment_text','pre_comment_id','article_id','comment_user','comment_time'))  #以键值对的形式取出评论对象，并且转化为列表list类型
-
         return JsonResponse(article,safe=False)   #JsonResponse返回JSON字符串，自动序列化，如果不是字典类型，则需要添加safe参数为False
     #else:
     #    return redirect('/user_login/')
