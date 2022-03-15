44fc957762f27478319cc7569bace4c3f6c51c32
White Mac <2641131b@student.gla.ac.uk>
2022-03-15 19:12:34 +0000

    fix null and space comment

--- comment/views.py
+++ comment/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .models import Comment
 from django.urls import reverse
+from django.contrib import messages
 
 # Create your views here.
 
@@ -30,10 +31,17 @@ def comment_control(request):
     if request.user.username:
         comment_user = request.user
         comment_text = request.POST.get('text','')
+
         article_id = request.POST.get('article_id')
+
+        if len(comment_text) == 0 or comment_text.isspace() :
+            print('输入无效评论')
+            messages.error(request,"Please enter valid content!")
+            return redirect('/comment/detail/'+ article_id)
+
         pid = request.POST.get('pid')
         Comment.objects.create(comment_text=comment_text,pre_comment_id=pid,article_id=article_id,comment_user=comment_user)
-
+        print('输入评论有效')
         article = list(Comment.objects.values('comment_id','comment_text','pre_comment_id','article_id','comment_user','comment_time'))
         return redirect('/comment/detail/'+ article_id)
     #else:
